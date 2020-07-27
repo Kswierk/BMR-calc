15031e4b89a77249af8cb1386992a233c484e976
Kswierk <kacper.swierk@gmail.com>
2020-07-27 16:46:54 +0200

    Calc form added

---
+++ js/navbar.js
@@ -0,0 +1,25 @@
+  const navSlide = () => {
+      const burger = document.querySelector('.burger');
+      const nav = document.querySelector('.navbar__list');
+      const navLinks = document.querySelectorAll('.navbar__item')
+
+      burger.addEventListener('click', () => {
+          nav.classList.toggle('navbar__list--active');
+
+          // navbar slide
+
+          navLinks.forEach((link, index) => {
+              if (link.style.animation) {
+                  link.style.animation = "";
+              } else {
+                  link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7}s`
+              }
+          });
+
+          // burger animation
+          burger.classList.toggle('toggle')
+      });
+
+  };
+
+  navSlide();
