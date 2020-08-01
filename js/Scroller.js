eb0ea78f25777187ab2b3c44bc35953abe2132da
Kswierk <kacper.swierk@gmail.com>
2020-08-01 11:54:00 +0200

    Update Scroller.js

--- js/Scroller.js
+++ js/Scroller.js
@@ -30,24 +30,32 @@ class Scroller {
         this.ageInput = document.querySelector('[data-age]');
         this.weightInput = document.querySelector('[data-weight]');
         this.heightInput = document.querySelector('[data-height]');
-
-        if (calc.age.value === "", calc.weight.value === "", calc.height.value === "") {
-            return this.ageInput.style.border = '1px solid red',
-                this.ageInput.setAttribute('placeholder', 'błędne dane'),
-                this.weightInput.style.border = '1px solid red',
-                this.weightInput.setAttribute('placeholder', 'błędne dane'),
-                this.heightInput.style.border = '1px solid red',
-                this.heightInput.setAttribute('placeholder', 'błędne dane')
-        }
-
         this.resultsSection = document.querySelector('.results');
+
+        this.badValues = calc.weight.value > 200 || calc.weight.value < 30 || calc.age.value > 100 || calc.age.value < 10 || calc.height.value > 250 || calc.height.value < 100;
+
+        this.emptyValues = calc.weight.value === "" || calc.height.value === "" || calc.age.value === "";
+
+        if (this.badValues) {
+            return this.ageInput.style.border = '1px solid red',
+                this.ageInput.setAttribute('placeholder', 'wpisz dane'),
+                this.weightInput.style.border = '1px solid red',
+                this.weightInput.setAttribute('placeholder', 'wpisz dane'),
+                this.heightInput.style.border = '1px solid red',
+                this.heightInput.setAttribute('placeholder', 'wpisz dane'),
+
+                this.hideResults();
+        }
 
         this.resultsSection.classList.add('show-results');
 
         this.ageInput.style.border = "1px solid black";
         this.weightInput.style.border = "1px solid black";
         this.heightInput.style.border = "1px solid black";
+
+
     }
+
 
     hideResults() {
         this.resultsSection.classList.remove('show-results');
